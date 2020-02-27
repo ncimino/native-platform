00cb8804fac77846ac39d9db190a93a1c3910306
Lóránt Pintér <lorant@gradle.com>
2020-02-27 15:17:30 +0000

    Clarify why case-insensitive test does not pass on Linux

--- src/test/groovy/net/rubygrapefruit/platform/file/AbstractFileEventsTest.groovy
+++ src/test/groovy/net/rubygrapefruit/platform/file/AbstractFileEventsTest.groovy
@@ -365,7 +365,7 @@ abstract class AbstractFileEventsTest extends Specification {
         expectedChanges.await()
     }
 
-    @IgnoreIf({ Platform.current().linux })
+    @Requires({ !Platform.current().linux })
     def "can receive events from directory with different casing"() {
         given:
         def lowercaseDir = new File(rootDir, "watch-this")
