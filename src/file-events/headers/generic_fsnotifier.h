0b6dddec5e6354619e88e6ab439080a83215c120
Lóránt Pintér <lorant@gradle.com>
2020-02-27 14:09:14 +0000

    Extract utf8ToUtf16String() to generic fsnotifier

--- src/file-events/headers/generic_fsnotifier.h
+++ src/file-events/headers/generic_fsnotifier.h
@@ -68,5 +68,7 @@ private:
 
 u16string javaToNativeString(JNIEnv* env, jstring javaString);
 
+u16string utf8ToUtf16String(const char* string);
+
 // TODO Use a template for the server type?
 jobject wrapServer(JNIEnv* env, function<void*()> serverStarter);
